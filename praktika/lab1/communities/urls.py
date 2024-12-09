91866286515a835f78e34a37fa2e01324c78dd6f
ST3511 <ST3511@TE.local>
2024-12-09 16:15:38 +0600

    complete

--- praktika/lab1/communities/urls.py
+++ praktika/lab1/communities/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'communities'
+
 urlpatterns = [
-    path('', views.communities_list, name='communities'),
+    path('', views.communities_list, name='list'),
 ]
--- praktika/lab1/communities/views.py
+++ praktika/lab1/communities/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
 from .models import Community
 
 def communities_list(req):
     communities = Community.objects.all()  # Получаем все группы из базы данных
     return render(req, 'communities/communities.html', {'communities': communities})
+
